e3253bb490d4c3be65cfd77049aa953017fadd18
John Pamisi <pamisijohn@gmail.com>
2024-09-27 18:57:24 +0100

    First commit

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {};
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
